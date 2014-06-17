e6fa88228fe8bc020a4c87dbd32364ce8688de4b
DavisLWebb <davislwebb@ymail.com>
2014-06-17 16:48:30 -0400

    Had to do some strange things

---
+++ config/application.rb
@@ -0,0 +1,41 @@
+require File.expand_path('../boot', __FILE__)
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module AlecsWebsite
+  class Application < Rails::Application
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+
+    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
+    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
+    # config.time_zone = 'Central Time (US & Canada)'
+
+    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    # config.i18n.default_locale = :de
+
+    Spud::Photos.configure do |config|
+      config.base_layout = 'application'
+      config.galleries_enabled = false
+      config.base_path = 'photos'
+      config.photo_styles = {
+        :small => '50x50#',
+        :medium => '200x200#',
+        :large => '400x400#',
+        :huge => '600x600'
+      }
+      config.max_image_upload_size = 0
+      config.paperclip_storage = :filesystem #use :s3 to use s3 storage (aws gem required)
+          config.s3_credentials = "#{Rails.root}/config/s3.yml"
+          config.storage_path = ":rails_root/public/system/spud_photos/:id/:style/:basename.:extension"
+          config.storage_url = "/system/spud_photos/:id/:style/:basename.:extension"
+    end
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+  end
+end
---
+++ db/schema.rb
@@ -0,0 +1,122 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20140529182612) do
+
+  create_table "spud_permissions", force: true do |t|
+    t.string   "name",       null: false
+    t.string   "tag",        null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "spud_permissions", ["tag"], name: "index_spud_permissions_on_tag", unique: true
+
+  create_table "spud_photo_albums", force: true do |t|
+    t.string   "title"
+    t.string   "url_name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "spud_photo_albums", ["url_name"], name: "idx_album_url_name"
+
+  create_table "spud_photo_albums_photos", force: true do |t|
+    t.integer "spud_photo_album_id"
+    t.integer "spud_photo_id"
+    t.integer "sort_order",          default: 0
+  end
+
+  add_index "spud_photo_albums_photos", ["spud_photo_album_id"], name: "idx_album_id"
+
+  create_table "spud_photo_galleries", force: true do |t|
+    t.string   "title"
+    t.string   "url_name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "spud_photo_galleries", ["url_name"], name: "idx_gallery_url_name"
+
+  create_table "spud_photo_galleries_albums", force: true do |t|
+    t.integer "spud_photo_gallery_id"
+    t.integer "spud_photo_album_id"
+    t.integer "sort_order",            default: 0
+  end
+
+  add_index "spud_photo_galleries_albums", ["spud_photo_gallery_id"], name: "idx_gallery_id"
+
+  create_table "spud_photos", force: true do |t|
+    t.string   "title"
+    t.string   "caption"
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
+    t.datetime "photo_updated_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "spud_role_permissions", force: true do |t|
+    t.integer  "spud_role_id",        null: false
+    t.string   "spud_permission_tag", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "spud_role_permissions", ["spud_permission_tag"], name: "index_spud_role_permissions_on_spud_permission_tag"
+  add_index "spud_role_permissions", ["spud_role_id"], name: "index_spud_role_permissions_on_spud_role_id"
+
+  create_table "spud_roles", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "spud_user_settings", force: true do |t|
+    t.integer  "spud_user_id"
+    t.string   "key"
+    t.string   "value"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "spud_users", force: true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.boolean  "super_admin"
+    t.string   "login",                           null: false
+    t.string   "email",                           null: false
+    t.string   "crypted_password",                null: false
+    t.string   "password_salt",                   null: false
+    t.string   "persistence_token",               null: false
+    t.string   "single_access_token",             null: false
+    t.string   "perishable_token",                null: false
+    t.integer  "login_count",         default: 0, null: false
+    t.integer  "failed_login_count",  default: 0, null: false
+    t.datetime "last_request_at"
+    t.datetime "current_login_at"
+    t.datetime "last_login_at"
+    t.string   "current_login_ip"
+    t.string   "last_login_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "time_zone"
+    t.integer  "spud_role_id"
+  end
+
+  add_index "spud_users", ["email"], name: "index_spud_users_on_email"
+  add_index "spud_users", ["login"], name: "index_spud_users_on_login"
+  add_index "spud_users", ["spud_role_id"], name: "index_spud_users_on_spud_role_id"
+
+end
